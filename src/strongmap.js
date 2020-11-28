2ce4efaaf612f6b8be33146fea1b73152f28f22b
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-11-28 05:10:46 +0000

    "Full proxification that's relatively efficient"

--- src/strongmap.js
+++ src/strongmap.js
@@ -1,49 +1,78 @@
 const name = "StrongMap";
 const HashTable = Map; // could also be WeakMap
+const T = Symbol('[[Target]]');
+const P = Symbol('[[Proxy]]');
 
 class StrongMap extends HashTable {};
+const GeneralFunction = function(...a) { return a; }
 
-class API {
-  get (target, prop, receiver) {
-    console.log(target, prop, receiver);
-    switch(prop) {
-      default:
-        if ( target[prop] instanceof Function ) {
-          return target[prop].bind(target);
-        }
-        return target[prop];
-        break;
+// static APIHandler
+  class StaticAPIHandler {
+    // constructor trap to ensure we return an instance that is a proxy
+    // so we can intercept its calls and put them to disk
+    construct(constructor, args, newTarget) {
+      const target = new StrongMap(...args);
+      const handler = new APIHandler(target);
+      return new Proxy(target, handler); 
     }
   }
 
-  apply (target, thisArg, args) {
-    console.log(target, thisArg, args);
-  }
-}
+// instance APICallHandler
+  class APICallHandler {
+    constructor(target, handler) {
+      this.target = target;
+      this.handler = handler;
+    }
 
-class StaticAPI {
-  construct(target, args, newTarget) {
-    return new Proxy(new StrongMap(...args), new API()); 
-  }
+    construct(target, args, newTarget) {
+      console.log(target, args, newTarget);
+      return new this.target[this.handler[T]](...args);
+    }
 
-  get (target, prop, receiver) {
-    console.log(target, prop, receiver);
-    switch(prop) {
-      case Symbol.species:
-        console.log("Symbol.species");
-        return StrongMap;
-      default:
-        return target[prop];
-        break;
+    apply (target, thisArg, args) {
+      console.log(target, thisArg, args);
+      console.log({
+        thisArg,                        // the proxy
+        target, 
+        args,
+        thisHandler: this.handler,      // the API handler
+        thisTarget: this.target         // the original object being proxied
+      });
+      return this.target[this.handler[T]].apply(this.target, args);
     }
   }
 
-  apply (target, thisArg, args) {
-    console.log(target, thisArg, args);
+// instance APIHandler
+  class APIHandler {
+    constructor(target) {
+      const callHandler = new APICallHandler(target, this);
+      const funcProxy = new Proxy(GeneralFunction, callHandler);
+      let funcName = null;
+      Object.defineProperty(this, T, {
+        get: () => funcName,
+        set: val => funcName = val
+      });
+      Object.defineProperty(this, P, {
+        get: () => funcProxy
+      });
+    }
+
+    get (target, prop, receiver) {
+      console.log(target, prop, receiver);
+      switch(prop) {
+        default:
+          if ( target[prop] instanceof Function ) {
+            // save the func name
+            this[T] = prop;
+            // return the func call proxy;
+            return this[P];
+          }
+          return target[prop];
+          break;
+      }
+    }
   }
-}
 
-
-const StrongMapStaticAPI = new Proxy(StrongMap, new StaticAPI());
+const StrongMapStaticAPI = new Proxy(StrongMap, new StaticAPIHandler());
 
 export default StrongMapStaticAPI;
